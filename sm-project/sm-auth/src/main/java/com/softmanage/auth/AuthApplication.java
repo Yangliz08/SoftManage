757945a7a2939f352fe0452fbeedec6644a46e82
liz <liz@123>
2026-06-11 17:34:14 +0800

    fix: correct auth application component scan setup

--- sm-project/sm-auth/src/main/java/com/softmanage/auth/AuthApplication.java
+++ sm-project/sm-auth/src/main/java/com/softmanage/auth/AuthApplication.java
@@ -4,12 +4,13 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @EnableDiscoveryClient
 @MapperScan("com.softmanage.auth.mapper")
-public class AuthApplication {
 @ComponentScan(basePackages = {"com.softmanage.auth", "com.softmanage.common"})
+public class AuthApplication {
     public static void main(String[] args) {
         SpringApplication.run(AuthApplication.class, args);
     }
